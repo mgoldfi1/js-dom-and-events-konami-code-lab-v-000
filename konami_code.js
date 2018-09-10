373ba775a3cc06978e316efc4a45cda338feca7e
Matthew Goldfine <mgoldfi1@binghamton.edu>
2018-09-10 19:14:21 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,7 +1,7 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 const body = document.querySelector('body')
 let index = 0
-function init(e) {
+function init() {
   return body.addEventListener('keydown', function(e){return console.log(e.which) })
-  
+
 }
