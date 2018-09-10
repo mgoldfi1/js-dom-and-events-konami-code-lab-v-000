d03b76e73679965e82da06e11cf2b379404b82e6
Matthew Goldfine <mgoldfi1@binghamton.edu>
2018-09-10 19:07:27 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,9 +1,7 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 const body = document.querySelector('body')
 let index = 0
-function init() {
-  // Write your JavaScript code inside the init() function
-  body.addEventListener('keydown', function(e){
+function init(e) {
     const key = parseInt(e.detail || e.which);
     if (key === code[index]) {
    index++;
@@ -16,3 +14,4 @@ function init() {
         index = 0;
     }
   }
+
